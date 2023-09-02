595e6326bafb9fe12b67ef13abc6c33ad07b552c
Jay9874 <jayprakashsharma225@gmail.com>
2023-09-02 13:40:47 +0530

    FIX: imports in App root

---
+++ src/components/Worker/index.js
@@ -0,0 +1,9 @@
+import Sidebar from "./Sidebar";
+import Tasks from "./Tasks";
+import Payment from "./Payment";
+import Attendance from "./Attendance";
+import Jobs from "./Jobs";
+import Profile from "./Profile";
+import Content from "./Content";
+
+export { Sidebar, Tasks, Payment, Attendance, Jobs, Profile, Content };
