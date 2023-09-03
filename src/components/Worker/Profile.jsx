6bb8d5e9e0c88f01be27e71f4b7262f1733ad7a7
Pranav Soan <pranav.soan.cs21@ggits.net>
2023-09-03 17:06:58 +0530

    safe commit

--- src/components/Worker/Profile.jsx
+++ src/components/Worker/Profile.jsx
@@ -15,37 +15,25 @@ const Profile = () => {
               <div className="profile-head">
                 <h2>John Doe</h2>
               </div>
+              <div className="flex justify-start gap-3">
+
+             
               <div className="profile-id">
                 <p>Worker ID: 123456789</p>
               </div>
+              <div className="profile-id">
+                <p>|Worker ID: 123456789</p>
+              </div>
+              </div>
             </div>
             <div className="col-md-2">
-              {/* Add the button with the "edit-profile-button" class */}
               <button className="btn btn-primary edit-profile-button">
                 Edit Profile
               </button>
             </div>
           </div>
-          {/* Center the fields lower in the white box */}
-          <div className="row justify-content-center"style={{marginBottom:"25px"}}>
-            <div className="col-md-12 text-center" >
-              <div className="profile-field" style={{ marginBottom: "20px" }}>
-                <label>ID:</label>
-                <span>123456</span>
-              </div>
-              <div className="profile-field" style={{ marginBottom: "20px" }}>
-                <label>Name:</label>
-                <span>John Doe</span>
-              </div>
-              <div className="profile-field" style={{ marginBottom: "20px" }}>
-                <label>DOB:</label>
-                <span>01/01/1990</span>
-              </div>
-              <div className="profile-field" style={{ marginBottom: "20px" }}>
-                <label>Aadhar Card:</label>
-                <span>1234-5678-9012</span>
-              </div>
-            </div>
+          <div className="row">
+          
           </div>
         </form>
       </div>
