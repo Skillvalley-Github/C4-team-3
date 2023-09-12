322d1d735382aee3cdd2d76e102f3f026f168e56
Akash Nai <akashnai632@gmail.com>
2023-09-12 21:33:32 +0530

    fixing env

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = `${__SUPABASE_URL__}`
-const supabaseKey = `${__SUPABASE_KEY__}`
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = `${import.meta.env.VITE_SUPABASE_URL}`
+const supabaseKey = `${import.meta.env.VITE_SUPABASE_KEY}`
 
-export default supabase
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
