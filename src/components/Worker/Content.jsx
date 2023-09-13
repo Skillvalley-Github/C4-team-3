eec286f46b9e8ae86dbaab2c72815b9abbe47b92
Pranav Soan <pranav.soan.cs21@ggits.net>
2023-09-13 21:42:29 +0530

    try commit

--- src/components/Worker/Content.jsx
+++ src/components/Worker/Content.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import Sidebar from './Sidebar'
 
 const Content = () => {
   return (
-    <div className='w-3/4 h-screen flex items-center justify-center font-bold text-5xl'>
-    
+    <div className='w-3/4 h-screen flex items-center justify-center font-bold text-5xl'>    
       
     </div>
   )
