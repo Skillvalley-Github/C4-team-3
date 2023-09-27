80267ff82e1438de7416193002c326d8036a2103
Jay9874 <jayprakashsharma225@gmail.com>
2023-09-28 04:25:30 +0530

    add calendar interal content

---
+++ src/utils/locationDrops.js
@@ -0,0 +1,12 @@
+export const district = [
+  { id: 1, name: "Wade Cooper" },
+  { id: 2, name: "Arlene Mccoy" },
+  { id: 3, name: "Devon Webb" },
+  { id: 4, name: "Tom Cook" },
+  { id: 5, name: "Tanya Fox" },
+  { id: 6, name: "Hellen Schmidt" },
+  { id: 7, name: "Caroline Schultz" },
+  { id: 8, name: "Mason Heaney" },
+  { id: 9, name: "Claudie Smitham" },
+  { id: 10, name: "Emil Schaefer" },
+];
