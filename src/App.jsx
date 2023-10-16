44d1f360d01a9b9d87ccd27f5eee29351137a8f7
Jay9874 <jayprakashsharma225@gmail.com>
2023-10-16 23:32:43 +0530

    Home sign in alingment

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     setJobs(data)
   }
   return (
-    <div className='w-full'>
+    <div className='w-full h-full'>
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/auth' element={<Auth />} />
